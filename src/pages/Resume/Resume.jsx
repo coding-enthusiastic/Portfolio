e623cacabc9db144bd978302f1b067d07db480e1
Mohammath Arfath <arafatharafath45@gmail.com>
2025-05-05 20:04:46 +0530

    updated resume loading

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "./Resume.module.css";
 
 function Resume() {
+  const [isLoading, setIsLoading] = useState(true);
+  const pdfLink = "/CV.pdf";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div className={styles.pdfScreen}>
-      <object
-        data="/src/assets/pdf/CV.pdf"
-        type="application/pdf"
-        width="100%"
-        height="1000px"
-      >
-        <p className={styles.pdfErrorText}>
-          Your browser does not support PDFs. You can{<br />}
-          <a href="/src/assets/pdf/CV.pdf">Download the PDF</a>.
-        </p>
-      </object>
+      {isLoading ? (
+        <p>Loading Resume...</p>
+      ) : (
+        <object
+          data={pdfLink}
+          type="application/pdf"
+          width="100%"
+          height="1000px"
+          onError={() => console.error("Failed to load PDF..!")}
+        >
+          <p className={styles.pdfErrorText}>
+            Your browser does not support PDFs. You can{<br />}
+            <a href="/CV.pdf">Download the PDF</a>
+          </p>
+        </object>
+      )}
     </div>
   );
 }
